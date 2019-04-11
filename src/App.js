00d039c5a8987df552eb1a068dbe56ad34fc74b4
Den Scollo <dens.scollo@gmail.com>
2019-04-11 15:54:58 -0300

    updatte app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
+import { DirectLine } from 'botframework-directlinejs';
+import ReactWebChat from 'botframework-webchat';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.directLine = new DirectLine({ token: 'lxF61qhvCmc.dAA.NQBBADkANAB4ADgAdAA4AEkARAAyADgARABEAHEAMQBNAGIAUQBPAEIARwAtADMA.1-rPJ5zw1AE.e1G9biwEPyM.imv-_317JzaRy2WznVmhdN6etbqIC803qM1rsb9hKIU' });
+  }
+  
   render() {
     return (
       <div className="App">
@@ -20,6 +28,7 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <ReactWebChat directLine={this.directLine} userID='30da83b6-d9a7-4455-9d8f-cf8b639a629e'/>
       </div>
     );
   }
